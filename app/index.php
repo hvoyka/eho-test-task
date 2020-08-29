ccfe496f08ed9388d6c902c403a057539e612fa1
Lavrentev Stanislav <hvoinii@gmail.com>
2020-08-29 12:19:31 +0600

    complite calc block

--- app/index.php
+++ app/index.php
@@ -48,13 +48,93 @@
           </li>
           <li class="breadcrumbs__item">
             <span>
-            <span class="breadcrumbs__item-link breadcrumbs__item-link--active">Калькулятор</span>
-            </span>            
+              <span class="breadcrumbs__item-link breadcrumbs__item-link--active">Калькулятор</span>
+            </span>
           </li>
         </ul>
       </div>
     </div>
+
+    <div class="calc">
+      <div class="container container__inner">
+        <div class="calc__wrapper">
+          <h1 class="calc__title">Калькулятор</h1>
+          <form class="calc__form">
+            <div class="calc__form-item">
+              <label for="calc__form-date" class="calc__form-label">
+                Дата оформления вклада:
+              </label>
+              <input id="calc__form-date" class="calc__form-input " type="date">
+            </div>
+            <div class="calc__form-item">
+              <label for="calc__form-sum" class="calc__form-label">
+                Сумма вклада:
+              </label>
+              <input id="calc__form-sum" class="calc__form-input" type="text" min="1000" max="3000000">
+            </div>
+
+            <div class="calc__form-item">
+              <label for="calc__form-time" class="calc__form-label">
+                Срок вклада:
+              </label>
+              <select id="calc__form-time" class="calc__form-input">
+                <option value="1">1 год</option>
+                <option value="2">2 год</option>
+                <option value="3">3 год</option>
+                <option value="4">4 год</option>
+                <option value="5">5 год</option>
+              </select>
+            </div>
+
+            <div class="calc__form-item">
+              <label class="calc__form-label">
+                Пополнение вклада:
+              </label>
+
+              <label>
+                <input type="radio" id="caclRadioDep1" name="calcDep" value="dep" checked>
+                Нет</label>
+
+              <label>
+                <input type="radio" id="caclRadioDep2" name="calcDep" value="dep">
+                да</label>
+            </div>
+            <div class="calc__form-item">
+              <label for="calc__form-sum-dep" class="calc__form-label">
+                Сумма пополнения вклада:
+              </label>
+              <input id="calc__form-sum-dep" class="calc__form-input" type="text" min="1000" max="3000000">
+            </div>
+            <div class="calc__form-item">
+              <button class="calc__form-btn">Рассчитать</button>
+              <span class="calc__form-result-text">
+                Результат:&nbsp;
+              </span>
+              <span class="calc__form-result-box">
+                <span id="calcFormValue">0</span>
+                &nbsp;руб
+              </span>
+            </div>
+          </form>
+        </div>
+      </div>
+
+    </div>
   </main>
+  <footer class="footer">
+    <div class="container">
+      <div class="footer__wrapper">
+        <ul class="footer-list">
+          <li class="footer-list__item"><a class="footer-list__item-link" href="#">Кредитные карты</a></li>
+          <li class="footer-list__item"><a class="footer-list__item-link" href="#">Вклады</a></li>
+          <li class="footer-list__item"><a class="footer-list__item-link" href="#">Дебетовая карта</a></li>
+          <li class="footer-list__item"><a class="footer-list__item-link" href="#">Страхование</a></li>
+          <li class="footer-list__item"><a class="footer-list__item-link" href="#">Друзья</a></li>
+          <li class="footer-list__item"><a class="footer-list__item-link" href="#">Интернет-банк</a></li>
+        </ul>
+      </div>
+    </div>
+  </footer>
   <script src="js/libs.min.js"></script>
   <script src="js/main.js"></script>
 </body>
