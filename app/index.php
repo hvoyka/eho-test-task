4fa23cd6d24b7c2c806875148ea45b150f228f4e
Lavrentev Stanislav <hvoinii@gmail.com>
2020-08-29 10:18:01 +0600

    fix names for importet files in style.scss, complite header

--- app/index.php
+++ app/index.php
@@ -10,7 +10,32 @@
 </head>
 
 <body>
-
+  <header class="header">
+    <div class="container">
+      <div class="header__top">
+        <div class="header__logo">
+          <a href="/"><img class="header__logo-img" src="./img/logo.png" alt="World Bank Publications logotype" title="World Bank Publications logotype"></a>
+        </div>
+        <div class="header__phones">
+          <a href="tel:+88001005005">8-800-100-5005</a>
+          <a href="tel:+73452522000">+7 (3452) 522-000</a>
+        </div>
+      </div>
+      <div class="header__bottom">
+        <button class="header__burger js-burger"></button>
+        <nav class="header__nav">
+          <ul class="header__nav-list nav-list">
+            <li class="nav-list__item"><a class="nav-list__item-link" href="#">Кредитные карты</a></li>
+            <li class="nav-list__item"><a class="nav-list__item-link nav-list__item-link--active" href="#">Вклады</a></li>
+            <li class="nav-list__item"><a class="nav-list__item-link" href="#">Дебетовая карта</a></li>
+            <li class="nav-list__item"><a class="nav-list__item-link" href="#">Страхование</a></li>
+            <li class="nav-list__item"><a class="nav-list__item-link" href="#">Друзья</a></li>
+            <li class="nav-list__item"><a class="nav-list__item-link" href="#">Интернет-банк</a></li>           
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </header>
 
   <script src="js/libs.min.js"></script>
   <script src="js/main.js"></script>
