0ccf66f070be170b59cba47830ec092e59b080a5
Lavrentev Stanislav <hvoinii@gmail.com>
2020-08-29 09:53:45 +0600

    create start template

---
+++ gulpfile.js
@@ -0,0 +1,86 @@
+let gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    rename = require('gulp-rename'),
+    del = require('del'),
+    autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('clean', async function(){
+  del.sync('dist')
+})
+
+gulp.task('scss', function(){
+  return gulp.src('app/scss/**/*.scss')
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(autoprefixer({
+      overRideBrowsers: ['last 10 versions'],
+    }))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('app/css'))
+    .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('css', function(){
+  return gulp.src([
+    'node_modules/normalize.css/normalize.css',   
+  ])
+    .pipe(concat('_libs.scss'))
+    .pipe(gulp.dest('app/scss'))
+    .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('script', function(){
+  return gulp.src('app/js/*.js')
+  .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('php', function(){
+  return gulp.src('app/*.php')
+  .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('js', function(){
+  return gulp.src([
+    'node_modules/jquery/dist/jquery.min.js'
+  ])
+    .pipe(concat('libs.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('app/js'))
+    .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('browser-sync', function() {
+  browserSync.init({
+      proxy: "http://eho/"
+  });
+});
+
+gulp.task('export', function(){
+
+  let BuildCss = gulp.src('app/css/**/*.css')
+    .pipe(gulp.dest('dist/css'));
+
+  let BuildJs = gulp.src('app/js/**/*.js')
+    .pipe(gulp.dest('dist/js'));
+    
+  let BuildFonts = gulp.src('app/fonts/**/*.*')
+    .pipe(gulp.dest('dist/fonts'));
+
+  let BuildImg = gulp.src('app/img/**/*.*')
+    .pipe(gulp.dest('dist/img'));
+
+  let BuildPhp = gulp.src('app/*.php')
+    .pipe(gulp.dest('dist/'));   
+});
+
+gulp.task('watch', function(){
+  gulp.watch('app/scss/**/*.scss', gulp.parallel('scss'));
+  gulp.watch('app/js/*.js', gulp.parallel('script'))
+  gulp.watch('app/*.php', gulp.parallel('php'))
+});
+
+gulp.task('build', gulp.series('clean', 'export'))
+
+gulp.task('default', gulp.parallel('css' ,'scss', 'js', 'php' , 'browser-sync', 'watch'));
